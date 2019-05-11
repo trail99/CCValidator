090193ec7bded9077c9738bb1d5dd2c96f292249
trail99 <anup.kodlekere@gmail.com>
2019-05-11 20:19:07 +0530

    Optimized.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
-
+ 
+// Calculates the length of the number 
 int numLen(unsigned long long n) {
   int count = 0;
   while (n != 0) {
@@ -10,7 +11,7 @@ int numLen(unsigned long long n) {
   }
   return count;
 }
-
+ 
 // Iterates through the number digit by digit and inserts at end
 // in numarray //
 void fillNumArray(int nArr[], unsigned long long num, int n) {
@@ -20,38 +21,42 @@ void fillNumArray(int nArr[], unsigned long long num, int n) {
     num /= 10;
   }
 }
-
-void addBits(int Arr[], int number, int i) {
+ 
+// Utility function to add the individual bits of a number
+// and replace that position with the new number i.e the sum
+void addBits(int Arr[], int i) {
   int sum = 0;
-  while(number != 0) {
-    sum += number % 10;
-    number /= 10;
+  while(Arr[i] != 0) {
+    sum += Arr[i] % 10;
+    Arr[i] /= 10;
   }
   Arr[i] = sum;
 }
-
+ 
+// Calculates the sum of the final array //
 int findSum(int arr[], int n) {
   int sum = 0;
   for(int i = 0; i < n; ++i) sum += arr[i];
   return sum;
 }
-
-bool luhn(int lArray[], int Array[], int num, int n) {
-  lArray[n] = {0};
+ 
+// Luhn Algorithm //
+bool luhn(int Array[], int num, int n) { 
   for(int i = n - 2; i >= 0; i = i - 2) {
-    lArray[i] = Array[i] * 2;
-    if(numLen(lArray[i] >= 2)) addBits(lArray, lArray[i], i);
+    // Double every second digit from right //
+    Array[i] *= 2; 
+    // If the product has more than two digits then add the individual digits
+    // using addBits() function //
+    if(numLen(Array[i] >= 2)) addBits(Array, i);  
   }
-  
-  for(int i = 0; i < n; ++i) {
-    if(lArray[i] == 0) lArray[i] = Array[i];
-  }
-  
-  int modSum = findSum(lArray, n);
+     
+  // Find the sum of the final array //
+  int modSum = findSum(Array, n);
   return (modSum % 10 == 0)? true : false;
-
+ 
 }
-
+ 
+// Finds the starting digit(s) of the CC //
 int getStartDigits(unsigned long long num, unsigned long long div) {
   int digits;
   digits = num / div;
@@ -61,35 +66,33 @@ int getStartDigits(unsigned long long num, unsigned long long div) {
   }
   return digits; 
 }
-
+ 
 int main(int argc, char** argv) {
   
-  char* pend;
+  // To convert the command line args to unsigned long long //
+  char* pend; 
   // Holds the credit card number to be verfied //
-  //unsigned long long number = 79927398713; //379927398713 //
   unsigned long long number = std::strtoull(argv[1], &pend, 10);
   int noOfDigits = numLen(number);
   unsigned long long divisor = pow(10, (noOfDigits - 1));
-  
+   
   // Card Number Length Check //
-  if(noOfDigits < 10 || noOfDigits > 16) {
+  if(noOfDigits < 13 || noOfDigits > 16) {
     std::cout<<"\n Card Length Error! \n";
     exit(0);
   }
-
+ 
   // Gets the starting digits (1 or 2) from the number 
   // for the identification of the card //
   int digits = getStartDigits(number, divisor);
   int numArray[noOfDigits]  = {0};
-  int luhnArray[noOfDigits] = {0};
- 
-  //std::cout << digits;
+      
   // Fill the array with each digit of the number //
   fillNumArray(numArray, number, noOfDigits);
-
+ 
   // Check whether the card is valid or not //
-  bool isValid = luhn(luhnArray, numArray, number, noOfDigits);
-
+  bool isValid = luhn(numArray, number, noOfDigits);
+ 
   if(isValid) {
     if(digits == 4)
       std::cout << "\n Your Visa card is valid!";
@@ -101,7 +104,7 @@ int main(int argc, char** argv) {
       std::cout << "\n Your Discover card is valid!";
     else std::cout << "\n Card is valid but not recognized!";
   } else std::cout << "\n You're a fraud!";
-
+ 
   return 0;
-
+ 
 }
